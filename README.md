b3fd3396ca226acc7f4f8e4dda65e179af711661
Nauman <naumansamtia3@gmail.com>
2021-07-29 18:14:29 +0500

    URL Package Test

---
+++ README.md
@@ -0,0 +1,20 @@
+# URL Launcher Package
+
+> Support IOS, Android and Web. Simple Code
+
+## Demo
+
+![](screenshot.png)
+
+
+## Getting Started
+clone this repo
+```
+git clone https://github.com/naumansamtia/url-launcher-test.git.git url_package_test
+```
+
+## Connect With Me
+Nauman Samtia– [@NaumanSamtia](https://www.linkedin.com/in/naumansamtia/) – naumansamtia3@gmail.com
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UrlLauncherPackage(),
+    );
+  }
+}
+
+class UrlLauncherPackage extends StatefulWidget {
+  const UrlLauncherPackage({Key? key}) : super(key: key);
+
+  @override
+  _UrlLauncherPackageState createState() => _UrlLauncherPackageState();
+}
+
+class _UrlLauncherPackageState extends State<UrlLauncherPackage> {
+  final String lat = "31.46604554302377";
+  final String lng = "74.25599821810253";
+
+  Future<void> _makePhoneCall(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could Not Launch $url";
+    }
+  }
+
+  _openWebURL() async {
+    const url = 'https://ioitechnologies.com';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could Not Launch $url";
+    }
+  }
+
+  _openMap() async {
+    final String googleMapsUrl = "comgooglemaps://?center=$lat,$lng";
+    final String myOfficeLocationUrl =
+        "https://g.page/ioiTechnologies?share=$lat,$lng";
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    }
+    if (await canLaunch(myOfficeLocationUrl)) {
+      await launch(myOfficeLocationUrl);
+    } else {
+      throw "Could Not Launch URL";
+    }
+  }
+
+  _openEmail() async {
+    launch(
+        "mailto:naumansamtia3@gmail.com?subject=TestingEmail&Body= I Am Mailing for Test");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("URL Testing"),
+      ),
+      body: Center(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45.0, vertical: 10.0),
+                child: Text(
+                  "Make Phone Call",
+                  style: TextStyle(color: Colors.green, fontSize: 18),
+                ),
+              ),
+              onPressed: () => setState(() {
+                _makePhoneCall('tel:+923461681066');
+              }),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _openWebURL();
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 45.0, vertical: 10.0),
+                child: Text(
+                  "Open Web URL",
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 18),
+                ),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _openEmail();
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 45.0, vertical: 10.0),
+                child: Text(
+                  "Open Email",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _openMap();
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 45.0, vertical: 10.0),
+                child: Text(
+                  "Open Map",
+                  style: TextStyle(color: Colors.red[300], fontSize: 18),
+                ),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
